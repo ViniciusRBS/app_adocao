64cc103e096ce2b8556c80f7668b196e365189c3
Vinicinho2013@ <viniborges704@gmail.com>
2025-07-07 17:17:57 -0300

    nova página de widget para botão de favorito

---
+++ lib/widgets/botao_favorito.dart
@@ -0,0 +1,63 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class BotaoFavorito extends StatefulWidget {
+  final String petId;
+
+  const BotaoFavorito({super.key, required this.petId});
+
+  @override
+  State<BotaoFavorito> createState() => _BotaoFavoritoState();
+}
+
+class _BotaoFavoritoState extends State<BotaoFavorito> {
+  bool _favoritado = false;
+  final user = FirebaseAuth.instance.currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    _verificarFavorito();
+  }
+
+  Future<void> _verificarFavorito() async {
+    if (user == null) return;
+    final doc = await FirebaseFirestore.instance.collection('pets').doc(widget.petId).get();
+    final List favoritos = doc['favoritadoPor'] ?? [];
+
+    setState(() {
+      _favoritado = favoritos.contains(user!.uid);
+    });
+  }
+
+  Future<void> _alternarFavorito() async {
+    if (user == null) return;
+    final docRef = FirebaseFirestore.instance.collection('pets').doc(widget.petId);
+    final doc = await docRef.get();
+
+    List favoritos = doc['favoritadoPor'] ?? [];
+
+    if (_favoritado) {
+      favoritos.remove(user!.uid);
+    } else {
+      favoritos.add(user!.uid);
+    }
+
+    await docRef.update({'favoritadoPor': favoritos});
+    setState(() {
+      _favoritado = !_favoritado;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        _favoritado ? Icons.favorite : Icons.favorite_border,
+        color: _favoritado ? Colors.red : Colors.white,
+      ),
+      onPressed: _alternarFavorito,
+    );
+  }
+}
